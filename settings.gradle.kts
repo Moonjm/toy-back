eb43bfb741b9737974e319ab9a663e8146cba3dd
moonjm <mym1207@naver.com>
2026-07-19 16:59:05 +0900

    feat: ledger 앱 모듈 스캐폴딩

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ project(":common-core").projectDir = file("common/core")
 project(":common-auth").projectDir = file("common/auth")
 project(":common-file").projectDir = file("common/file")
 
-include(":daily-record", ":family-tree")
+include(":daily-record", ":family-tree", ":ledger")
 project(":daily-record").projectDir = file("apps/daily-record")
 project(":family-tree").projectDir = file("apps/family-tree")
+project(":ledger").projectDir = file("apps/ledger")
